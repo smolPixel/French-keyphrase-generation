4cada7c9f9e8dbb635f85ec1beaecee2a837ae23
Frederic <bemeraude@hotmail.com>
2022-04-15 10:29:13 -0400

    auto commit

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch import Tensor
 from process_data import *
-from data.NoteMarg import initialize_NoteMarg
+from util import initialize_datasets
 from AugmentStrat.Augmentator import Augmentator
+from Model.BART import BARTModel
 
 def run_external_process(process):
     output, error = process.communicate()
@@ -37,51 +38,10 @@ def set_seed(seed=42):
 
 
 def run(argdict):
-    # generate_sentence = argdict['classifier']=='gpt'
-    createFolders(argdict)
-    check_data_selected(argdict)
-    augmentator=Augmentator(argdict)
-    #TODO HERE YOU WANT IT TO WORK EVEN IF SPLIT IS 0
-    dd=augmentator.augment()
-    #Build the dataset with augmented data
     datasets = initialize_NoteMarg(argdict, False)
-    # print(len(dd['train']))
-    if argdict['classifier']=='LogReg':
-        from Model.LogReg import LogReg
-        model=LogReg(argdict, datasets)
-    elif argdict['classifier'].lower()=='tfidf':
-        from Model.TFIDF import TFIDFModel
-        model=TFIDFModel(argdict, datasets)
-    elif argdict['classifier'].lower()=='yake':
-        from Model.YAKE import YAKEModel
-        model=YAKEModel(argdict, datasets)
-    if argdict['classifier']=='lstm':
-        from Model.Seq_to_seq import Encoder, Attention, Decoder, Seq2Seq
-        model = Seq2Seq(argdict, datasets)
-    elif argdict['classifier']=='WordsLSTM':
-        from Model.WordSeq_to_Seq import WordSeq2Seq
-        model= WordSeq2Seq(argdict, datasets)
-    elif argdict['classifier'].lower()=='bart':
-        from Model.BART import BARTModel
-        model=BARTModel(argdict, datasets)
-    #We don't talk about this
-    # elif argdict['classifier'] == 'bert':
-    #     from Model.BertSeq_to_seq import BertSeq2Seq
-    #     model = BertSeq2Seq(argdict, dd.datasets)
-    elif argdict['classifier'] == 'gpt':
-        from Model.GPT2 import GPT2Model
-        model=GPT2Model(argdict, datasets)
-    elif argdict['classifier']=='dummy':
-        from Model.dummy import dummy
-        model=dummy(argdict, dd.datasets)
-    elif argdict['classifier']=='opennmt':
-        from Model.OpenNMT import OpenNMT
-        model=OpenNMT(argdict, datasets)
-    elif argdict['classifier'].lower()=='gan':
-        from Model.GAN import GAN
-        model=GAN(argdict, datasets)
-    if argdict['embedding'] in ['glove']:
-        model.init_embeds()
+    print(datasets)
+    dsa
+    model=BARTModel(argdict, datasets)
     model.train_model()
     model.generate_from_dataset(split='train')
     model.generate_from_dataset(split='dev')
@@ -90,73 +50,19 @@ def run(argdict):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='VAE for data augmentation')
+    parser = argparse.ArgumentParser(description='Using Barthez for keyphrase generation')
     #General arguments on training
     parser.add_argument('--dataset', type=str, default='NoteMarginales', help="dataset you want to run the process on. Includes NoteMarginales, NoteMarginalesFr")
-    parser.add_argument('--language', type=str, default='en', help="Language of the dataset")
-    parser.add_argument('--classifier', type=str, default='lstm', help="classifier you want to use. Includes lstm, gpt, WordsLSTM, or dummy")
     parser.add_argument('--computer', type=str, default='labo', help="Whether you run at home or at iro. Automatically changes the base path")
-    parser.add_argument('--split', type=float, default=0, help='percent of the dataset added')
-    parser.add_argument('--retrain', action='store_true', help='whether to retrain the VAE or not')
-    parser.add_argument('--rerun', action='store_true', help='whether to rerun knowing that is has already been ran')
-    parser.add_argument('--dataset_size', type=int, default=10, help='number of example in the original dataset. If 0, use the entire dataset')
     parser.add_argument('--algo', type=str, default='None', help='data augmentation algorithm to use, includes, EDA, ConstraintVAE, VAE, HierarchicalVAE')
     parser.add_argument('--random_seed', type=int, default=7, help='Random seed ')
-    parser.add_argument('--device', type=str, default='cuda', help='cuda or cpu')
-    parser.add_argument('--strategy', type=str, default='one2many', help='one2one or one2many')
-
-    # Constraint VAE parameters
-    parser.add_argument('--nb_epoch_algo', type=int, default=30, help='Number of epoch of the algo')
-    parser.add_argument('--batch_size_algo', type=int, default=8, help='batch size of the data augmentation algo')
-    parser.add_argument('--latent_size', type=int, default=5, help='Latent Size')
-    parser.add_argument('--hidden_size_algo', type=int, default=1024, help='Hidden Size Algo')
-    parser.add_argument('--dropout_algo', type=float, default=0.3, help='dropout of the classifier')
-    parser.add_argument('--word_dropout', type=float, default=0.3, help='dropout of the classifier')
-    parser.add_argument('--x0', default=2500, type=int, help='x0')
-    parser.add_argument('--k', default=0.0025, type=float, help='k')
-    parser.add_argument('--min_vocab_freq', default=1, type=int, help='min freq of vocab to be included')
-
     parser.add_argument('--num_epochs', type=int, default=10, help='Number of epochs')
-    parser.add_argument('--num_epochs_secondary', type=int, default=10, help='Number of epochs for secondary mechanisme (VAE in VAE_GPT)')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch Size')
     parser.add_argument('--max_seq_length', type=int, default=64, help='max length, 0 if no max length')
-    parser.add_argument('--embedding_size', type=int, default=300, help='size of the embedding')
-    parser.add_argument('--embedding', type=str, default='none', help='either glove or none')
-    parser.add_argument('--hidden_size', type=int, default=64, help='size of the hidden size')
-    parser.add_argument('--dropout', type=float, default=0.3, help='dropout')
+
 
     parser.add_argument('--short_eval', action='store_true', help='Cut evaluation time for debugging purposes')
 
-
-
-    # parser.add_argument('--path', type=str, default="/media/frederic/DAIro", help="Empirical Path to the folder")
-    # parser.add_argument('--computer', type=str, default="home", help="home or labo")
-
     args = parser.parse_args()
-
     argsdict = args.__dict__
-
-
-    if argsdict['dataset'] in ['NoteMarginalesFr']:
-        argsdict['language']='fr'
-
-    if argsdict['classifier'] in ['bert', 'gpt', 'bart']:
-        print("transformers model detected, setting tokenizer to bert")
-        argsdict['embedding']='bert'
-
-    if argsdict['classifier'] in ['bart']:
-        print("Bart Model detected, setting preprocessing to bart")
-        argsdict['preprocessing']='bart'
-    elif argsdict['classifier'] in ['gpt']:
-        print("GPT Model detected, setting preprocessing to gpt")
-        argsdict['preprocessing']='gpt'
-    else:
-        argsdict['preprocessing']=None
-
-
-    if argsdict['computer']=='home':
-        argsdict['path']='/media/frederic/DAIro'
-    elif argsdict['computer']=='labo':
-        argsdict['path']='/u/piedboef/Documents/DAIro'
-
     run(argsdict)
---
+++ util.py
@@ -0,0 +1,155 @@
+"""Contain the data processor for Note Marginal for pytorch"""
+from torch.utils.data import Dataset
+import os, io
+import numpy as np
+import json
+import pandas as pd
+from collections import defaultdict, OrderedDict, Counter
+from nltk.tokenize import TweetTokenizer, sent_tokenize
+from torchtext.vocab import build_vocab_from_iterator
+
+
+
+class OrderedCounter(Counter, OrderedDict):
+    """Counter that remembers the order elements are first encountered"""
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
+
+    def __reduce__(self):
+        return self.__class__, (OrderedDict(self),)
+
+
+def initialize_datasets(argdict, generate_sentences=False):
+    #Generate sentences: Prepare the dataset to generate sentences from the marginal notes
+    train=pd.read_csv("train.tsv", sep='\t', index_col=0)
+    train=train.dropna()
+    dev=pd.read_csv("dev.tsv", sep='\t', index_col=0)
+    allsentences=list(train['sentences'])
+    allsentences.extend(list(train['label']))
+    tokenizer=TweetTokenizer()
+    allsentences=[tokenizer.tokenize(sentence) for sentence in allsentences if sentence==sentence]
+    vocab = build_vocab_from_iterator(allsentences, min_freq=argdict['min_vocab_freq'], specials=["<unk>", "<pad>", "<bos>", "<eos>"], )
+    vocab.set_default_index(vocab["<unk>"])
+    train=NoteMarg(train, tokenizer, vocab, argdict, generate_sentences=generate_sentences)
+    dev=NoteMarg(dev, tokenizer, vocab, argdict, dev=True, generate_sentences=generate_sentences)
+    return train, dev
+
+class NoteMarg(Dataset):
+
+    def __init__(self, data, tokenizer, vocab, argdict, dev=False, generate_sentences=False):
+        super().__init__()
+        """data: tsv of the data
+           tokenizer: tokenizer trained
+           vocabInput+Output: vocab trained on train"""
+        self.data = {}
+        self.max_len = argdict['max_seq_length']
+        self.vocab = vocab
+        self.tokenizer=tokenizer
+        # self.pad_idx = self.vocab['<pad>']
+        self.max_len_label=0
+        self.max_len_words=0
+        self.num_sentences=0
+        self.max_len_labels=0
+        self.len_sentence=0
+        self.index_unique_examples=[]
+        self.generate_sentence = generate_sentences
+        index=0
+        self.map_unique_to_id={}
+        for i, row in data.iterrows():
+            if dev and argdict['short_eval'] and index>10:
+                break
+            if row['sentences'] in ['.', '', ' '] or row['label'] in ['.', '', ' ']:
+                continue
+            # if self.len_sentence<max([len(sent) for sent in sentences_sep]):
+            #     self.len_sentence=max([len(sent) for sent in sentences_sep])
+            try:
+                tokenized_text = self.tokenizer.tokenize("<bos> " + row['sentences'] + " <eos>")
+            except:
+                print("bish")
+                print(row)
+                fds
+            input = np.array(vocab(tokenized_text))
+            self.index_unique_examples.append(index)
+            self.map_unique_to_id[index]=[]
+
+            if argdict['one_kp']:
+                labs=[row['label']]
+            else:
+                labs=row['label'].split(', ')
+            # gptvae_sentence = ""
+
+            # print(labs)
+            # print(row['sentences']
+            sents=sent_tokenize(row['sentences'])
+            sents_trunc=[""]
+            for ss in sents:
+                if len(ss[-1].split(" "))+len(ss.split(" "))>argdict['max_seq_length']:
+                    sents_trunc.append(ss)
+                else:
+                    sents_trunc[-1]+=ss+" "
+            for sent in sents_trunc:
+                sentence_max_len = sent
+                if generate_sentences:
+                    sent_gpt = "<bos> " + row['label'].lower() + " <sep> " + row['sentences'].lower() + " <eos>"
+                    # gptvae_sentence="<bos> "+row['sentences'].lower() +" <sep> "+row['label'].lower()+" <eos>"
+                    All_Labels_Tokenized = np.array(
+                        vocab(self.tokenizer.tokenize("<bos> " + row['label'] + " <eos>")))
+                    # print(All_Labels_Tokenized)
+                    if len(All_Labels_Tokenized) > self.max_len_labels:
+                        self.max_len_labels = len(All_Labels_Tokenized)
+                else:
+                    All_Labels_Tokenized = [0]
+                    sent_gpt = "test"
+                labs = row['label']
+                gpt_sentence = "<bos> " + sentence_max_len.lower() + " <sep> " + labs.lower() + " <eos>"
+                tokenized_labels = self.tokenizer.tokenize("<bos> " + labs + " <eos>")
+                self.data[index] = {'input': input, 'label': tokenized_labels,
+                                    'NoteMarginale': labs,
+                                    'full_labels': row['label'], 'input_sentence': row['sentences']}
+                self.map_unique_to_id[self.index_unique_examples[-1]].append(index)
+                index+=1
+
+    def tokenize(self, batch):
+        """tokenize a batch"""
+        results=[]
+        for sent in batch:
+            tokenized_text = self.tokenizer.tokenize(sent)
+            results.append(self.vocab(tokenized_text))
+
+        return results
+
+    @property
+    def vocab_size(self):
+        return len(self.vocab)
+
+    @property
+    def eos_idx(self):
+        return self.vocab['<eos>']
+
+    @property
+    def pad_idx(self):
+        return self.vocab['<pad>']
+
+    @property
+    def bos_idx(self):
+        return self.vocab['<bos>']
+
+    @property
+    def unk_idx(self):
+        return self.vocab['<unk>']
+
+    def get_i2w(self):
+        return self.vocab.get_itos()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return {
+            'input_sentence':self.data[item]['input_sentence'],
+            'full_labels':self.data[item]['full_labels'],
+    }
+
+    def iterexamples(self):
+        for i, ex in self.data.items():
+            yield i, ex

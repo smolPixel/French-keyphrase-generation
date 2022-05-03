ce36e8d18e787c492314574a303226e761e62ad3
Frederic <bemeraude@hotmail.com>
2022-05-03 15:20:59 -0400

    auto commit

--- Model/YAKE.py
+++ Model/YAKE.py
@@ -46,27 +46,29 @@ class YakeModel():
 		r10 = np.average(score['absent_exact_recall@10'])
 		print(f"f1@10 present and r@10 absent for dev: {f10}, {r10}")
 
-		inputs = []
-		refs = []
-		hypos = []
-		for i, exos in tqdm(self.test_set['test'].data.items()):
-			# print(exos)
-			inputs.append(exos['input_sentence'])
-			refs.append([rr.strip() for rr in exos['full_labels'].split(',')])
-			if self.kw_extractor is None:
-				try:
-					kw_extractor=yake.KeywordExtractor(lan=exos['language'])
-				except:
-					kw_extractor = yake.KeywordExtractor()
-			else:
-				kw_extractor=self.kw_extractor
-			gend=kw_extractor.extract_keywords(exos['input_sentence'])
-			hypos.append([kw[0] for kw in gend])
+		for tt in self.test_set:
+			print(f"running for {tt} dataset")
+			inputs = []
+			refs = []
+			hypos = []
+			for i, exos in tqdm(self.tt.data.items()):
+				# print(exos)
+				inputs.append(exos['input_sentence'])
+				refs.append([rr.strip() for rr in exos['full_labels'].split(',')])
+				if self.kw_extractor is None:
+					try:
+						kw_extractor=yake.KeywordExtractor(lan=exos['language'])
+					except:
+						kw_extractor = yake.KeywordExtractor()
+				else:
+					kw_extractor=self.kw_extractor
+				gend=kw_extractor.extract_keywords(exos['input_sentence'])
+				hypos.append([kw[0] for kw in gend])
 
-		score = evaluate(inputs, refs, hypos, '<unk>', tokenizer='split_nopunc')
-		f10 = np.average(score['present_exact_f_score@10'])
-		r10 = np.average(score['absent_exact_recall@10'])
-		print(f"f1@10 present and r@10 absent for test: {f10}, {r10}")
+			score = evaluate(inputs, refs, hypos, '<unk>', tokenizer='split_nopunc')
+			f10 = np.average(score['present_exact_f_score@10'])
+			r10 = np.average(score['absent_exact_recall@10'])
+			print(f"f1@10 present and r@10 absent for test: {f10}, {r10}")
 
 	def generate_special_ex(self):
 		print(f"Generation for example 26XXX")

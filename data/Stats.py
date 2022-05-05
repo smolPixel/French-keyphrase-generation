3832176a24f3936fa24a15361ffb1c904b15c163
Frederic <bemeraude@hotmail.com>
2022-05-05 10:58:59 -0400

    auto commit

---
+++ data/Stats.py
@@ -0,0 +1,5 @@
+"""Some stats on the test corpora"""
+
+datasets=['papyrus_f', 'papyrus_e', 'papyrus_m', 'papyrus', 'WikiNews',
+		  'cacic57', 'wicc78', '110ptbnkp', 'pak2018', 'kp20k', 'Inspec',
+		  'NUS', 'SemEval', 'Krapivin']

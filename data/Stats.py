301a553e14092a785970150b827b1df671d3260d
Frederic <bemeraude@hotmail.com>
2022-05-05 13:49:07 -0400

    auto commit

--- data/Stats.py
+++ data/Stats.py
@@ -1,5 +1,18 @@
 """Some stats on the test corpora"""
+import pandas as pd
+import numpy as np
 
 datasets=['papyrus_f', 'papyrus_e', 'papyrus_m', 'papyrus', 'WikiNews',
 		  'cacic57', 'wicc78', '110ptbnkp', 'pak2018', 'kp20k', 'Inspec',
-		  'NUS', 'SemEval', 'Krapivin']
+		  'NUS', 'SemEval', 'krapivin']
+
+for dataset in datasets:
+	df=pd.read_csv(f"{dataset}/test.tsv", sep='\t', index_col=0)
+	try:
+		ll=list(df['language'])[0]
+	except:
+		ll='en'
+
+	sents=list(df['sentences'])
+	sents=[len(sent.split(' ')) for sent in sents]
+	print(f"Dataset {dataset}. Language {ll} Number of test exos: {len(df)} of average length {np.mean(sents)}")

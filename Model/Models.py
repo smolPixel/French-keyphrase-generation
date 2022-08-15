329cd15f8ca70bda613df71f60eba95a2868f08b
Frederic <bemeraude@hotmail.com>
2022-08-15 09:13:33 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -11,11 +11,11 @@ class SeqToSeq(torch.nn.Module):
 		self.embeddings=torch.nn.Embedding(self.argdict['input_size'], self.argdict['embed_size'])
 		self.rnn_decoder=torch.nn.GRU(self.argdict['embed_size'], self.argdict['hidden_size'], 1, batch_first=True, bidirectional=False)
 
-		self.loss=
+		self.loss=torch.nn.CrossEntropyLoss(ignore_index=argdict['pad_idx'])
 
 	def forward(self, input_seq, output_seq):
 		embed_in=self.embeddings(input_seq)
 		_, hidden=self.rnn_encoder(embed_in)
 		embed_out=self.embeddings(output_seq[:-1])
 		outputs=self.rnn_decoder(embed_out)
-		fds
+

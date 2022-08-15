39863e7bcb13e93ccc1ad282f42d6fb8c338c45e
Frederic <bemeraude@hotmail.com>
2022-08-15 14:50:06 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -29,7 +29,7 @@ class SeqToSeq(torch.nn.Module):
 		else:
 			return {'logits':outputs}
 
-	def generate(self, input_seq, num_beams=10, num_return_sequence=1, max_length=50):
+	def generate(self, input_seq, num_beams=10, num_return_sequences=1, max_length=50):
 		curr=torch.zeros((1, 1))+self.argdict['bos_idx']
 		embed_in=self.embeddings(input_seq)
 		_, hidden=self.rnn_encoder(embed_in)

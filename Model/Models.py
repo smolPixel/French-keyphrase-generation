5c388e76bfa315a8b9ecb6fdc6a3d38ff72f1883
Frederic <bemeraude@hotmail.com>
2022-08-16 15:45:37 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -7,7 +7,7 @@ class SeqToSeq(torch.nn.Module):
 		super().__init__()
 		self.argdict=argdict
 		self.embeddings=torch.nn.Embedding(self.argdict['input_size'], self.argdict['embed_size'])
-		self.rnn_encoder=torch.nn.GRU(self.argdict['embed_size'], self.argdict['hidden_size'], 2, batch_first=True, bidirectional=False)
+		self.rnn_encoder=torch.nn.GRU(self.argdict['embed_size'], self.argdict['hidden_size'], 1, batch_first=True, bidirectional=False)
 		"""Decoder"""
 		self.embeddings=torch.nn.Embedding(self.argdict['input_size'], self.argdict['embed_size'])
 		self.rnn_decoder=torch.nn.GRU(self.argdict['embed_size'], self.argdict['hidden_size'], 1, batch_first=True, bidirectional=False)

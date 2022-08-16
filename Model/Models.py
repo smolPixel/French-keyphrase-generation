314f52ff7d62ba56aa815be6a4afebbacf918b04
Frederic <bemeraude@hotmail.com>
2022-08-16 15:23:35 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -52,7 +52,8 @@ class SeqToSeq(torch.nn.Module):
 
 			#Update the curr index
 			#First we need to find for each topk from which branch it came
-			for value in top:
+			print(top)
+			for value, index in top:
 				print(value)
 				print(value, index)
 

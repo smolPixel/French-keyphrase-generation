d4ba5b34d134b978aa5745a17ee3a03badde6608
Frederic <bemeraude@hotmail.com>
2022-08-18 09:17:18 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -27,7 +27,7 @@ class SeqToSeq(torch.nn.Module):
 		print(outputs.shape)
 		print(outputs.view(-1, outputs.shape[-1]).shape)
 		print(target.shape)
-		print(target.view(-1).shape)
+		print(target.view(target.shape[0]*target.shape[1]).shape)
 		if output_seq is not None:
 			loss=self.loss(outputs.view(-1, outputs.shape[-1]), target.view(-1))
 			return {'logits':outputs, 'loss':loss}

c3fd3fa8931934c009a19bca3238b99a8bbeb801
Frederic <bemeraude@hotmail.com>
2022-08-18 16:03:16 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -31,9 +31,10 @@ class SeqToSeq(torch.nn.Module):
 			return {'logits':outputs}
 
 	def generate(self, input_seq, num_beams=10, num_return_sequences=1, max_length=50, device='cpu'):
+		bs=input_seq.shape[0]
 		curr=torch.zeros((input_seq.shape[0], num_beams, 1))+self.argdict['bos_idx']
 		# curr_log_prob=torch.zeros((10, 1))
-		curr_log_prob=torch.Tensor([0,1,2,3,4,5,6,7,8,9])
+		curr_log_prob=torch.zeros(bs, num_beams)
 		curr_log_prob=curr_log_prob.unsqueeze(1).to(device)
 		curr=curr.int().to(device)
 		print(input_seq.shape)

3705c2c266c9489c08ec7bf6a8e5d64a7ca68bfc
Frederic <bemeraude@hotmail.com>
2022-08-21 14:55:55 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -55,7 +55,7 @@ class SeqToSeq(torch.nn.Module):
 			print(curr_log_prob.shape)
 			print(outputs.shape)
 			phrase_log_prob=curr_log_prob+outputs
-			phrase_log_prob=phrase_log_prob.view(bs*num_beams, -1)
+			phrase_log_prob=phrase_log_prob.view(bs, -1)
 			top=torch.topk(phrase_log_prob, k=num_beams, dim=-1)
 			print(top.indices.shape)
 			fds

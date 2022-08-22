36d1d3e9ed924c1c73e81ce52c6f1508180142b4
Frederic <bemeraude@hotmail.com>
2022-08-22 08:49:09 -0400

    auto commit

--- Model/Models.py
+++ Model/Models.py
@@ -66,9 +66,10 @@ class SeqToSeq(torch.nn.Module):
 			#
 			new_log_prob=torch.zeros_like(curr_log_prob)
 			new_index=torch.zeros((curr.shape[0], curr.shape[1], curr.shape[2]+1))
-			for og_branch, new_ind, log_prob_new in zip(x, y, values):
-				print(og_branch, new_ind, log_prob_new)
-				print('---')
+			for i, og_branch, new_ind, log_prob_new in enumerate(zip(x, y, values)):
+				new_log_prob=curr_log_prob[i]+log_prob_new
+			print(new_log_prob)
+			fds
 			#
 			# for value, index in zip(top.values.squeeze(0), top.indices.squeeze(0)):
 			# 	#We need to find from which branch it comes

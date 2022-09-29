ed7d47bcc639aee566627e5201ff6db3ecf2cd96
Frederic <bemeraude@hotmail.com>
2022-09-29 10:41:47 -0400

    auto commit

--- parse_results.py
+++ parse_results.py
@@ -25,7 +25,7 @@ seeds=[42,43,44]
 datasets=['wicc78', '110ptbnkp', 'wikinews', 'cacic57', 'pak2018', 'papyruse', 'papyrusf', 'papyrusm', 'papyrus', 'kp20k', 'semeval', 'krapivin', 'nus', 'inspec']
 
 for seed in seeds:
-	file=open(f"Results/t5_papyrus_f_seed_{seed}.txt").read()
+	file=open(f"Results/t5_papyrus_e_seed_{seed}.txt").read()
 	# file=open(f"seed{seed}.txt").read()
 	file=file.split('Percent of absent generated keyphrases')[:-1]
 	# file=file.split('Running test for')[1:]
